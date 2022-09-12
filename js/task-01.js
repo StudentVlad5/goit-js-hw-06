124a95cf693c1c59a24c4817336c1f699fe8fb47
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-09-12 08:18:16 +0300

    main

--- js/task-01.js
+++ js/task-01.js
@@ -9,3 +9,9 @@ for(let i = 0; i < numberOfCategories.length; i += 1) {
     console.log(`Elements: ` + numberOfCategories[i].querySelectorAll(`ul > li`).length);
     console.log(`Elements (by the children): ` + numberCategoryEl[i].children.length);
 }
+
+// const list = document.querySelector('#categories');
+// const items = list.querySelectorAll('.item');
+// console.log(`В списке ${items.length} категории:`);
+// items.forEach(node => {const h2 = node.querySelector('h2');const nodeItems = node.querySelectorAll('li');
+// console.log(` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`);}); 
